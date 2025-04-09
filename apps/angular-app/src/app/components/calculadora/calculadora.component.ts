daec8a98a89dc7aced72ea0d87b358100f4cb5c0
Jorge-Neto <neto_tunas@hotmail.com>
2025-04-08 23:00:28 -0300

    feat(Angular) increased form calculator validators

--- apps/angular-app/src/app/components/calculadora/calculadora.component.ts
+++ apps/angular-app/src/app/components/calculadora/calculadora.component.ts
@@ -17,10 +17,10 @@ export class CalculadoraComponent {
   constructor(private fb: FormBuilder, private resultadoService: ResultadoService, private snackBar: MatSnackBar) {
     this.form = this.fb.group({
       marca: ['comum'],
-      anoFabricacao: [null, [Validators.required, Validators.min(1900)]],
-      anoModelo: [null, [Validators.required, Validators.max(2050)]],
+      anoFabricacao: [null, [Validators.required, Validators.min(1900), Validators.max(2049), Validators.pattern(/^\d+$/)]],
+      anoModelo: [null, [Validators.required, Validators.min(1901), Validators.max(2050), Validators.pattern(/^\d+$/)]],
       seguradora: ['nenhuma'],
-      lote: ['', [Validators.required]],
+      lote: ['', [Validators.required, Validators.pattern(/[a-zA-Z0-9/ ]+/)]],
       sinistro: ['pequena'],
       custosPercentual: [0.1],
       fipe: ['', Validators.required],
@@ -59,22 +59,26 @@ export class CalculadoraComponent {
       taxaSeguradora = 80;
     }
 
-    let custoMarca = marca === "premium" ? 0.1 * Number(fipe) : 0;
+    const fipeValor = Number(fipe);
+    const seguroValor = Number(seguro) || 0;
+    const multasValor = Number(multas) || 0;
+
+    let custoMarca = marca === "premium" ? 0.1 * fipeValor : 0;
     let custoAno = (2025 - anoFabricacao) * 50;
-    let custos = Number(fipe) * Number(custosPercentual) + custoMarca + custoAno;
+    let custos = fipeValor * Number(custosPercentual) + custoMarca + custoAno;
 
     let descontoMin = sinistro === "pequena" ? 0.2 : 0.4;
     let descontoMax = sinistro === "pequena" ? 0.4 : 0.6;
 
-    let precoRevendaMin = Number(fipe) * (1 - descontoMax);
-    let precoRevendaMax = Number(fipe) * (1 - descontoMin);
+    let precoRevendaMin = fipeValor * (1 - descontoMax);
+    let precoRevendaMax = fipeValor * (1 - descontoMin);
 
     // DSAL - Paraná (R$ 300,00)
     // Taxa de entrega (R$ 100,00)
     // Transferência de propriedade (R$ 130,61)
     // Vistoria veicular (R$ 129,33)
     // Título de despesas (R$ 386,00)
-    let despesasFixas = 300 + 100 + 130.61 + 129.33 + 386 + seguro + multas + taxaSeguradora;
+    let despesasFixas = 300 + 100 + 130.61 + 129.33 + 386 + seguroValor + multasValor + taxaSeguradora;
     let taxaLeilao = 0.05;
 
     this.valorArremateMin = (precoRevendaMin - custos - despesasFixas) / (1 + taxaLeilao);
